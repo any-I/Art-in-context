cf4537c78b2cbf446dce8eb748f6f6880a77e914
mrcsua <mjau@usc.edu>
2025-03-24 00:31:11 -0700

    query and requirements updated

--- services/pinecone_query.py
+++ services/pinecone_query.py
@@ -15,29 +15,38 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 
-pc = Pinecone(api_key=PINECONE_API_KEY)
+def query_index(query, index_name):
 
-index_name = "quickstart3"
+    pc = Pinecone(api_key=PINECONE_API_KEY)
+    index = pc.Index(index_name)
 
-index = pc.Index(index_name)
+    embedding = pc.inference.embed(
+        model="multilingual-e5-large",
+        inputs=[query],
+        parameters={
+            "input_type": "query"
+        }
+    )
+
+    results = index.query(
+        namespace="ns1",
+        vector=embedding[0].values,
+        top_k=3,
+        include_values=False,
+        include_metadata=True
+    )
+
+    return results
 
 
-query = "Tell me about the tech company known as Apple."
+def print_results(results):
+    for match in results['matches']:
+        print(f"{match['metadata']['title']}, {match['id']}")
+        print(match['metadata']['text'])
+        print()
 
-embedding = pc.inference.embed(
-    model="multilingual-e5-large",
-    inputs=[query],
-    parameters={
-        "input_type": "query"
-    }
-)
-
-results = index.query(
-    namespace="ns1",
-    vector=embedding[0].values,
-    top_k=3,
-    include_values=False,
-    include_metadata=True
-)
-
-print(results)
+if __name__ == "__main__":
+    index_name = "json-cleaned-sample"
+    query = "Claude Monet and Impressionism"
+    results = query_index(query, index_name)
+    print_results(results)    
